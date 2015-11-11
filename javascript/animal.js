e59cb33442dcfa6a2eed8f87bbde4b6f29bce0b6
Cho S. Kim <choskim.me@gmail.com>
2015-11-10 18:01:27 -0800

    Add solution for javascript/animal.js

--- javascript/animal.js
+++ javascript/animal.js
@@ -1,9 +1,35 @@
-function Animal(){
-  // write code here
+function Animal(kind, sound, name, age){
+  this.kind  = kind;
+  this.sound = sound;
+  this.name  = name;
+  this.age   = age;
+  this.awake = false;
 }
 
-Animal.prototype  // write more code here
+Animal.prototype.wakeUp = function() {
+  this.awake = true;
+};
 
+Animal.prototype.speak = function() {
+  return this.sound;
+};
 
+Animal.prototype.feed = function() {
+  var fedSound;
+
+  if (this.awake) {
+    fedSound = 'NOM NOM NOM';
+  }
+
+  return fedSound;
+};
+
+Animal.prototype.growUp = function() {
+  return this.age++;
+};
+
+Animal.prototype.sleep = function() {
+  this.awake = false;
+};
 
 module.exports = Animal;
