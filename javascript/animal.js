f913f63a586f1e5f315e76af8fe9d59f1bc028dc
Dustin Wurtz <dtwurtz@gmail.com>
2016-02-24 18:06:45 -0800

    All test pass

--- javascript/animal.js
+++ javascript/animal.js
@@ -1,9 +1,24 @@
-function Animal(kind){
+function Animal(kind, sound, name, age, awake){
   // write code here
   this.kind = kind;
+  this.sound = sound;
+  this.name = name;
+  this.age = age;
+  this.awake = false;
 }
 
-Animal.prototype  // write more code here
+Animal.prototype.wakeUp = function() {
+  this.awake = true;
+}
+Animal.prototype.speak = function() {
+  return this.sound;
+}
+Animal.prototype.feed = function() {
+  if (this.awake) {
+    return 'NOM NOM NOM';
+  }
+}
+// write more code here
 
 
 
--- javascript/zoo.js
+++ javascript/zoo.js
@@ -1,11 +1,40 @@
 var Animal = require('./animal');
 
-function Zoo() {
+function Zoo(location, name, status, animal) {
  // write code here
+ this.location = location;
+ this.name = name;
+ this.status = 'closed';
+ this.animals = [];
 }
 
 
-Zoo.prototype // write more code here
+Zoo.prototype.isOpen = function() {
+  if(this.status = 'closed') {
+    return false;
+  }
+}
+Zoo.prototype.open = function() {
+  this.status = 'open';
+}
+Zoo.prototype.addAnimal = function(animal) {
+  if (this.status === 'open' && animal instanceof Animal) {
+    this.animals.push(animal);
+  }
+}
+Zoo.prototype.removeAnimal = function (animal) {
+  if (this.status === 'open' && animal instanceof Animal) {
+    this.animals.splice(animal, 1);
+  }
+}
+Zoo.prototype.changeLocation = function () {
+  return this.location;
+}
+Zoo.prototype.close = function() {
+  if (this.status === 'open') {
+    return 'closed';
+  }
+} // write more code here
 
 
 module.exports = Zoo;
